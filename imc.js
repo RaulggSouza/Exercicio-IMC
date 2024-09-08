9f2ae56cfff89b4c055675493705629f121d9fec
RaulggSouza <raulggsouza@hotmail.com>
2024-09-08 15:42:27 -0300

    Implementacao de todos os botoes do menu opcoes da tabela

--- imc.js
+++ imc.js
@@ -18,7 +18,7 @@ altura.addEventListener("input",() => {
     }
 })
 btnEnviar.addEventListener("click",() => {
-    let imc = (peso.value/Math.pow(altura.value,2)).toFixed(2);
+    let imc = calcularImc(peso.value,altura.value);
     if (checarNumero(peso.value, "p") && checarNumero(altura.value, "a")){
         let table = document.querySelector("table");
         let tr = document.createElement("tr");
@@ -32,24 +32,23 @@ btnEnviar.addEventListener("click",() => {
         let div = document.createElement("div");
         div.setAttribute("class", "botoes");
         td.append(div);
+        
         let btnExcluir = document.createElement("button");
         btnExcluir.setAttribute("class", "excluir");
         btnExcluir.innerText = "Excluir";
-        btnExcluir.addEventListener("click", () => {
-            btnExcluir.parentElement.parentElement.remove();
-        })
+        btnExcluir.addEventListener("click", excluirLinha);
         div.append(btnExcluir);
 
         let btnMaisPeso = document.createElement("button");
         btnMaisPeso.setAttribute("class","maisPeso");
         btnMaisPeso.innerText = "+ Peso";
-        
+        btnMaisPeso.addEventListener("click", aumentarPeso);
         div.append(btnMaisPeso);
         
         let btnMenosPeso = document.createElement("button");
         btnMenosPeso.setAttribute("class","menosPeso");
         btnMenosPeso.innerText = "- Peso";
-
+        btnMenosPeso.addEventListener("click", diminuirPeso);
         div.append(btnMenosPeso);
         
         tr.append(td);
@@ -64,14 +63,52 @@ btnEnviar.addEventListener("click",() => {
 })
 function checarNumero(valor, inputTestado){
     if (inputTestado == "p"){
-        if (valor > 0.00 && valor < 300.00){
+        if (valor > 0.00 && valor <= 500.00){
             return true;
         }
     }
-    if (valor > 0.00 && valor < 3.00){
+    if (valor > 0.00 && valor <= 3.00){
         return true;
     }
 }
+
+function calcularImc(valorP, valorA){
+    return (valorP/Math.pow(valorA,2)).toFixed(2);
+}
+
+function excluirLinha(e){
+    let btnExcluir = e.target;
+    btnExcluir.parentElement.parentElement.parentElement.remove();
+}
+
+function aumentarPeso(e){
+    let btnMaisPeso = e.target;
+    let linhaEscolhida = btnMaisPeso.parentElement.parentElement.parentElement;
+    let valorPeso = parseFloat(linhaEscolhida.children[1].innerText);
+    let valorAltura = parseFloat(linhaEscolhida.children[2].innerText);
+    if (checarNumero(valorPeso+0.5, "p")){
+        linhaEscolhida.children[1].innerHTML = valorPeso+0.5;
+    }else{
+        alert("Peso máximo atingido");
+    }
+    linhaEscolhida.children[3].innerHTML = calcularImc(valorPeso+0.5, valorAltura);
+    linhaEscolhida.children[4].innerHTML = definirStatus(calcularImc(valorPeso+0.5, valorAltura));
+}
+
+function diminuirPeso(e){
+    let btnMenosPeso = e.target;
+    let linhaEscolhida = btnMenosPeso.parentElement.parentElement.parentElement;
+    let valorPeso = parseFloat(linhaEscolhida.children[1].innerText);
+    let valorAltura = parseFloat(linhaEscolhida.children[2].innerText);
+    if (checarNumero(valorPeso-0.5, "p")){
+        linhaEscolhida.children[1].innerHTML = valorPeso-0.5;
+    }else{
+        alert("Peso mínimo atingido");
+    }
+    linhaEscolhida.children[3].innerHTML = calcularImc(valorPeso-0.5, valorAltura);
+    linhaEscolhida.children[4].innerHTML = definirStatus(calcularImc(valorPeso-0.5, valorAltura));
+}
+
 function definirStatus(imc){
     if(imc < 18.5) {
         return "<span class='magreza'>Magreza</span>";
